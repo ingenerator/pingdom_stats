64d67bef4729031ed2bf20e0925cf7a0108f2c43
Andrew Coulton <andrew@ingenerator.com>
2014-03-05 10:18:37 +0000

    Remove reference to env.php
    Environment variables are set directly by the build

--- import_pingdom_uptimes.php
+++ import_pingdom_uptimes.php
@@ -6,8 +6,6 @@
  * @copyright 2014 inGenerator Ltd
  * @licence   BSD
  */
-
-require_once(__DIR__.'/env.php');
 require_once(__DIR__.'/vendor/autoload.php');
 require_once(__DIR__.'/src/JSONRequester.php');
 require_once(__DIR__.'/src/StatisticsAggregator.php');
